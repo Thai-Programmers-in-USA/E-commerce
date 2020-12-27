a8a8f29d0e9b0a4dc828c39d40daf62f140a52d9
BayLadyCoder <rachada_32@hotmail.com>
2020-12-26 19:46:56 -0500

    added theme and more sample code on Header.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,39 @@
-import './App.css';
+import {MuiThemeProvider} from '@material-ui/core/styles';
+import theme from './assets/theme/theme';
+import withStyles from '@material-ui/core/styles/withStyles';
+
 import Home from './pages/Home';
 import Header from './components/Header'
 import Footer from './components/Footer'
 
-function App() {
+function App({classes}) {
   return (
-    <div className="App">
-      <Header />
-        <Home />
-      <Footer />
-    </div>
+      <MuiThemeProvider theme={theme}>
+        <div className={classes.container}>
+          <Header />
+            <Home />
+          <Footer />
+        </div>
+      </MuiThemeProvider>
+
   );
 }
 
-export default App;
+const styles = ({palette, breakpoints, spacing,typography}) => ({
+    container: {
+      margin: "0 auto",
+      // width: "95%",
+      padding: '10px',
+      textAlign: "center",
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      justifyContent: "flex-start",
+      [breakpoints.up('md')]: {
+      padding: '20px',
+       
+      },
+    }
+});
+
+export default withStyles(styles)(App)
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,15 +4,25 @@ import withStyles from '@material-ui/core/styles/withStyles';
 import NavBar from './NavBar'
 import Typography from '@material-ui/core/Typography';
 import SearchBar from '../SearchBar';
-
+import MenuIcon from '@material-ui/icons/Menu';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import LocalShippingIcon from '@material-ui/icons/LocalShipping';
 
  const Header =({classes}) => {
     return (
         <div className={classes.container}>
         <div  className={classes.headerTopContainer}>
-            <Typography variant="body" component="p">EMPEROR</Typography>
+            <MenuIcon className={classes.menuIcon} />
+            <Typography variant="body1" component="p" className={classes.logo}>EMPEROR</Typography>
             <SearchBar />
-            <Typography variant="body" component="p">Cart</Typography>
+            <div className={classes.freeShippingContainer}>
+                <LocalShippingIcon className={classes.truckIcon} />
+                <div className={classes.freeShippingTextContainer}>
+                    <Typography variant="body1" component="p" className={classes.freeShipping}>Free Shipping</Typography>
+                    <Typography variant="body1" component="p" className={classes.allOrders}>on all orders over $75</Typography>
+                </div>
+            </div>
+            <ShoppingCartIcon className={classes.ShoppingCartIcon} />
         </div>
         <NavBar />
         </div>
@@ -23,14 +33,59 @@ Header.propTypes = {
   classes: PropTypes.object.isRequired,
 };
 
-const styles = {
+const styles = ({palette, breakpoints, spacing,typography}) => ({
     container: {
-        width: '100%'
+        width: '100%',
     },
     headerTopContainer: {
         display: "flex",
-        justifyContent:'space-between'
+        justifyContent:'space-between',
+        alignItems: 'center'
     },
-};
+    menuIcon: {
+        display: 'in-line',
+        [breakpoints.up('sm')]: {
+            display: 'none',
+        }
+    },
+    logo: {
+        color: palette.primary.main,
+        fontWeight: '800',
+        fontSize: '30px'
+    },
+    freeShippingContainer: {        
+        display: 'none',
+
+        [breakpoints.up('sm')]: {
+        display: 'flex',
+        justifyContent:'center',
+        alignItems: 'flex-end',
+        }
+    },
+    truckIcon: {
+        fontSize: '40px',
+        color: palette.secondary.main
+    },
+    freeShippingTextContainer: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems:'flex-start',
+        margin: '0 15px',
+        color: palette.secondary.main
+    },
+    freeShipping: {
+        fontSize: '14px',
+        fontWeight: 'bold'
+    },
+    allOrders: {
+        fontSize: '13px',
+    },
+    ShoppingCartIcon: {
+        fontSize: '35px',
+        color: palette.secondary.main
+    }
+
+
+});
 
 export default withStyles(styles)(Header)
