67c647f6a1101d455689a8a791e232439adc9332
Kato <kubkate@gmail.com>
2020-12-27 18:18:14 -0800

    use Grid in FeaturedCategories

--- src/components/Features/FeaturedCard.js
+++ src/components/Features/FeaturedCard.js
@@ -1,10 +1,25 @@
-import React from 'react'
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import { blueGrey } from '@material-ui/core/colors';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    backgroundColor: blueGrey
+  }
+}));
 
 const FeaturedCard = () => {
+  const classes = useStyles();
+
     return (
-        <div>
-            Featured Card
-        </div>
+      <Card className={classes.root}>
+        <h2>Title</h2>
+            <p> 
+              Discover the latest designer, modern and contemporary furniture and accessories from around the world.
+            </p>
+            <button>Click Me!</button>
+      </Card>
     )
 }
 
--- src/components/Features/FeaturedCategories.js
+++ src/components/Features/FeaturedCategories.js
@@ -1,10 +1,34 @@
 import React from 'react'
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
 import FeaturedCard from './FeaturedCard';
 
+const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+      backgroundColor: '#4d4d4d'
+    }
+  }));
+
 const FeaturedCategories = () => {
+    const classes = useStyles();
+
     return (
-        <div>
-            <FeaturedCard />
+        <div className={classes.root}>
+            <Grid container spacing={2}>
+                 <Grid item xs={8}>
+                    <FeaturedCard />
+                </Grid>
+                <Grid item xs={4}>
+                    <FeaturedCard />
+                </Grid>
+                <Grid item xs={4}>
+                    <FeaturedCard />
+                </Grid>
+                <Grid item xs={8}>
+                    <FeaturedCard />
+                </Grid>
+            </Grid> 
         </div>
     )
 }
