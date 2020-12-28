4874a4bc93bc01fbefaebd5238b305601b21ba06
Kato <kubkate@gmail.com>
2020-12-27 17:32:38 -0800

    setup FeaturedCard

---
+++ src/components/Features/FeaturedCard.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FeaturedCard = () => {
+    return (
+        <div>
+            Featured Card
+        </div>
+    )
+}
+
+export default FeaturedCard;
--- src/components/Features/FeaturedCategories.js
+++ src/components/Features/FeaturedCategories.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import FeaturedCard from './FeaturedCard';
 
 const FeaturedCategories = () => {
     return (
         <div>
-            Featured Categories
+            <FeaturedCard />
         </div>
     )
 }
