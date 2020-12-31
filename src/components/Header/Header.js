ba75c77c46a363f8a13ff2af1d977c73c1c756be
BayLadyCoder <rachada_32@hotmail.com>
2020-12-31 12:35:16 -0500

    Added Nav Bar (static)

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -18,19 +18,19 @@ import IconButton from '@material-ui/core/IconButton';
             <Typography variant="body1" component="p" className={classes.logo}>EMPEROR</Typography>
             <SearchBar />
             <div className={classes.freeShippingAndCartContainer}>
-            <div className={classes.freeShippingContainer}>
-                <LocalShippingIcon className={classes.truckIcon} />
-                <div className={classes.freeShippingTextContainer}>
-                    <Typography variant="body1" component="p" className={classes.freeShipping}>Free Shipping</Typography>
-                    <Typography variant="body1" component="p" className={classes.allOrders}>on all orders over $75</Typography>
+                <div className={classes.freeShippingContainer}>
+                    <LocalShippingIcon className={classes.truckIcon} />
+                    <div className={classes.freeShippingTextContainer}>
+                        <Typography variant="body1" component="p" className={classes.freeShipping}>Free Shipping</Typography>
+                        <Typography variant="body1" component="p" className={classes.allOrders}>on all orders over $75</Typography>
+                    </div>
                 </div>
+                <IconButton aria-label="cart" className={classes.cartIconButton}>
+                    <Badge badgeContent={3} color="primary" className={classes.cartBadge}>
+                        <ShoppingCartIcon className={classes.ShoppingCartIcon} />
+                    </Badge>
+                </IconButton>
             </div>
-            <IconButton aria-label="cart" className={classes.cartIconButton}>
-                <Badge badgeContent={3} color="primary" className={classes.cartBadge}>
-                    <ShoppingCartIcon className={classes.ShoppingCartIcon} />
-                </Badge>
-             </IconButton>
-                </div>
         </div>
         <NavBar />
         </div>
@@ -41,91 +41,94 @@ Header.propTypes = {
   classes: PropTypes.object.isRequired,
 };
 
-const styles = ({palette, breakpoints, spacing,typography}) => ({
-    container: {
-        width: '100%',
-        boxShadow: '0px 2px 4px #f0f0f0',
-        padding: '5px 15px',
-        [breakpoints.up('sm')]: {
-            padding: '15px 25px',
-        }
+const styles = ({ palette, breakpoints, spacing, typography }) => ({
+  container: {
+    width: "100%",
+    boxShadow: "0px 2px 4px #f0f0f0",
+    padding: "5px 15px",
+    [breakpoints.up("sm")]: {
+      padding: "15px 25px",
     },
-    headerTopContainer: {
-        display: "flex",
-        justifyContent:'space-between',
-        alignItems: 'center',
+  },
+  headerTopContainer: {
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    padding: "0px 15px 5px 15px",
+    [breakpoints.up("sm")]: {
+      padding: "0px 25px 15px 25px",
     },
-    menuIcon: {
-        display: 'in-line',
-        [breakpoints.up('sm')]: {
-            display: 'none',
-        }
+  },
+  menuIcon: {
+    display: "in-line",
+    [breakpoints.up("sm")]: {
+      display: "none",
     },
-    logo: {
-        color: palette.primary.main,
-        fontWeight: '800',
-        fontSize: '30px',
+  },
+  logo: {
+    color: palette.primary.main,
+    fontWeight: "800",
+    fontSize: "30px",
+  },
+  freeShippingAndCartContainer: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  freeShippingContainer: {
+    display: "none",
+    [breakpoints.up(680)]: {
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
     },
-    freeShippingAndCartContainer: {
-        display: 'flex',
-        justifyContent:'center',
-        alignItems: 'center',
+  },
+  truckIcon: {
+    fontSize: "38px",
+    color: palette.secondary.main,
+  },
+  freeShippingTextContainer: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "flex-start",
+    margin: "0 15px",
+    color: palette.secondary.main,
+  },
+  freeShipping: {
+    fontSize: "14px",
+    fontWeight: "bold",
+  },
+  allOrders: {
+    fontSize: "13px",
+  },
+  cartIconButton: {
+    padding: 0,
+    "&:hover": {
+      backgroundColor: "transparent",
     },
-    freeShippingContainer: {        
-        display: 'none',
-        [breakpoints.up(680)]: {
-        display: 'flex',
-        justifyContent:'center',
-        alignItems: 'center',
-        }
-    },
-    truckIcon: {
-        fontSize: '38px',
-        color: palette.secondary.main,
-    },
-    freeShippingTextContainer: {
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems:'flex-start',
-        margin: '0 15px',
-        color: palette.secondary.main,
-    },
-    freeShipping: {
-        fontSize: '14px',
-        fontWeight: 'bold'
-    },
-    allOrders: {
-        fontSize: '13px',
-    },
-    cartIconButton:{
-        padding: 0,
-        '&:hover': {
-            backgroundColor: 'transparent'
-        }
-    },
-    ShoppingCartIcon: {
-        fontSize: '35px',
-        color: palette.secondary.main, 
-    },
-  '@global': {
+  },
+  ShoppingCartIcon: {
+    fontSize: "35px",
+    color: palette.secondary.main,
+  },
+  "@global": {
     '[class*="MuiBadge-badge"]': {
-        border: `2px solid #ffffff`,
-        padding: '7px 2px',
-        borderRadius: '50%',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: '13px',
-        [breakpoints.up('sm')]: {
-            padding: '8px 4px',
-            fontSize: '13px',
-        },
-        [breakpoints.up('md')]: {
-            padding: '10px 6px',
-            fontSize: '15px',
-        },
+      border: `2px solid #ffffff`,
+      padding: "7px 2px",
+      borderRadius: "50%",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      fontSize: "13px",
+      [breakpoints.up("sm")]: {
+        padding: "8px 4px",
+        fontSize: "13px",
+      },
+      [breakpoints.up("md")]: {
+        padding: "10px 6px",
+        fontSize: "15px",
+      },
     },
-
   },
 });
 
--- src/components/Header/NavBar/NavBar.js
+++ src/components/Header/NavBar/NavBar.js
@@ -1,11 +1,64 @@
 import React from 'react'
+import withStyles from "@material-ui/core/styles/withStyles";
+import Link from "@material-ui/core/Link";
+import Typography from "@material-ui/core/Typography";
 
- const NavBar = () => {
+ const NavBar = ({classes}) => {
     return (
-        <div>
-            Nav Bar
-        </div>
-    )
+      <div className={classes.container}>
+        <Typography className={classes.typography}>
+          <Link href="#" className={classes.link}>
+            Home
+          </Link>
+          <Link href="#" className={classes.link}>
+            Shop
+          </Link>
+          <Link href="#" className={classes.link}>
+            About
+          </Link>
+          <Link href="#" className={classes.link}>
+            Contact
+          </Link>
+        </Typography>
+      </div>
+    );
 }
 
-export default NavBar;
+const styles = ({ palette, breakpoints }) => ({
+  container: {
+      display: 'none',
+    [breakpoints.up("sm")]: {
+      display: "flex",
+      paddingLeft: "25px",
+    },
+  },
+  link: {
+    color: "#363636",
+    fontWeight: "700",
+    fontSize: "14px",
+    "&:hover": {
+      color: "#787878",
+      textDecoration: "none",
+    },
+    [breakpoints.up("sm")]: {
+      fontSize: "16px",
+    },
+  },
+  typography: {
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    width: "100%",
+    [breakpoints.up("sm")]: {
+      width: "50%",
+    },
+    [breakpoints.up("md")]: {
+      width: "40%",
+    },
+    [breakpoints.up("lg")]: {
+      width: "30%",
+    },
+  },
+});
+
+export default withStyles(styles)(NavBar);
