3c10a5b70c31bf5f045f70cd02a5b02a83eb5c97
Kato <kubkate@gmail.com>
2021-01-04 18:48:42 -0800

    implement styleGenerator function

--- src/components/Features/FeaturedCard.js
+++ src/components/Features/FeaturedCard.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Card, CardContent, Button, Typography, makeStyles } from '@material-ui/core';
 
-const useStyles = makeStyles({
+const defaultStyles = {
   root: {
     minHeight: 260,
     // justify: "end"
@@ -10,15 +10,50 @@ const useStyles = makeStyles({
   cardContentStyle: {
     marginBottom: "25px"
   },
+  typographyStyles: {
+    color: "yellow"
+  },
   btnStyles: {
-    color: "blue", // text
-    backgroundColor: "blue"
+    color: "white", // text
+    backgroundColor: "red"
   }
-});
+}
+
+const textGray = {
+  typographyStyles: {
+    color: "gray"
+  }
+}
+
+const btnWhite = {
+  btnStyles: {
+    color: "red", // text
+    backgroundColor: "gray"
+  }
+}
+
+const styleGenertor = (syleOption) => {
+  switch (syleOption) {
+    case 'option1':
+      return {
+        ...defaultStyles,
+        ...textGray
+      };
+    case 'option2':
+    return {
+      ...defaultStyles,
+      ...btnWhite
+    };
+    default:
+      return defaultStyles
+  }
+}
 
 const FeaturedCard = ({ featureData }) => {
+  // passin props style
+  const featuredCardStyles = styleGenertor('option2');
+  const useStyles = makeStyles(featuredCardStyles);
   const classes = useStyles();
-  // TODO: destructure style for a unique botton color
   const { data } = featureData;
   const bgImage = {  
     backgroundImage: `url(${data.imgURL})`,
@@ -26,20 +61,28 @@ const FeaturedCard = ({ featureData }) => {
     backgroundSize: "100% auto",
     backgroundPosition: "center center",
     // backgroundAttachment: "fixed"
-    
   }
   
   // TODO: make link button redirect to page
   return (
     <Card style={bgImage} align="left" className={classes.root}>
       <CardContent className={classes.cardContentStyle}>
-        <Typography variant="h5" component="h2">
+        <Typography 
+          className={classes.typographyStyles} 
+          variant="h5" 
+          component="h2"
+        >
           { data.title }
         </Typography>
-        <Typography variant="subtitle2" component="p" >
+        <Typography 
+          className={classes.typographyStyles} variant="subtitle2" 
+          component="p" 
+        >
           { data.desc }
         </Typography>
-         <Button variant="contained" color="primary" size="small" >{ data.linkName }</Button>
+         <Button className={classes.btnStyles}>
+          { data.linkName }
+        </Button>
       </CardContent>
     </Card>
   )
