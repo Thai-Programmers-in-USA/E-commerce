55cbfd7ca7fbac7ab222c2677e10263acea49ea8
Parada <paradafaii28@gmail.com>
2021-01-06 13:22:40 -0700

    create instagram and style color

--- src/components/ProductCard/Icon.js
+++ src/components/ProductCard/Icon.js
@@ -1,8 +1,8 @@
 import React from "react"
 import withStyles from '@material-ui/core/styles/withStyles';
 import { SiPinterest } from "react-icons/si";
-import { FaTwitterSquare, FaFacebook, FaLinkedin} from "react-icons/fa";
-import { AiFillTwitterCircle } from "react-icons/ai";
+import {FaFacebook} from "react-icons/fa";
+import { AiFillTwitterCircle, AiOutlineInstagram} from "react-icons/ai";
 
 
 const Icon = ({classes}) => {
@@ -11,7 +11,7 @@ const Icon = ({classes}) => {
             <ul className={classes.iconContainer}>
                 <li className={classes.icon}><FaFacebook color='#435DAB' size='16px'/></li>
                 <li className={classes.icon}><AiFillTwitterCircle color='#0077B5' size='20px'/></li>
-                <li className={classes.icon}><FaLinkedin color='#0077B5' size='16px' /></li>
+                <li className={classes.icon}><AiOutlineInstagram className={classes.iconIg} color='#ffffff' size='20px' border-radius='50%'/></li>
                 <li className={classes.icon}><SiPinterest color='#BD1C1B' size='16px' /></li>
             </ul>
         </div>
@@ -27,6 +27,10 @@ const styles = ({palette, breakpoints}) => ({
     },
     icon: {
         paddingRight: "10px"
+    },
+    iconIg: {
+        backgroundImage: "linear-gradient(to bottom right, purple, red, purple)",
+        borderRadius: "50%"
     }
 
 })
