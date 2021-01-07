0636f0d374b48294b17eed587b08feff8efed8b0
Nuanjan <Maine09102016>
2021-01-07 00:02:45 -0500

    add icon to footer and styling the responsive

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,8 +6,11 @@ import withStyles from '@material-ui/core/styles/withStyles';
 import PropTypes from 'prop-types';
 import Typography from '@material-ui/core/Typography';
 import useMediaQuery from '@material-ui/core/useMediaQuery';
-import { DragHandleOutlined } from '@material-ui/icons';
-
+import FacebookIcon from '@material-ui/icons/Facebook';
+import TwitterIcon from '@material-ui/icons/Twitter';
+import PinterestIcon from '@material-ui/icons/Pinterest';
+import InstagramIcon from '@material-ui/icons/Instagram';
+import EmailIcon from '@material-ui/icons/Email';
 
 const Footer = ({classes}) => {
   const isBigScreen = useMediaQuery('(min-width:970px)');
@@ -61,23 +64,25 @@ const handleCollapse = (section) => {
           <div className={classes.subRowNoDropdown}>
             <button className={classes.footerBtn}>Follow us</button>
             <div className={classes.iconList}>
-            <p>Icon</p>
-            <p>Icon</p>
-            <p>Icon</p>
-            <p>Icon</p>
-            <p>Icon</p>
+            <p className={classes.subIconList}><FacebookIcon/></p>
+            <p className={classes.subIconList}><TwitterIcon/></p>
+            <p className={classes.subIconList}><PinterestIcon/></p>
+            <p className={classes.subIconList}><InstagramIcon/></p>
+            <p className={classes.subIconList}><EmailIcon/></p>
             </div>
             
           </div>
           <div className={classes.subRowNoDropdown}>
             <button className={classes.footerBtn}>Subscribe</button>
             <Typography className={classes.textSubscribe} variant="subtitle1" component="p">Sign up to receive 15% off your first order!</Typography>
+            <div className={classes.subscribeSignUp}>
             <input className={classes.signUpInput}/>
             <input type="submit" value="sign up" className={classes.signUpBtn} />
+            </div>
           </div>
          
         </div>
-        <div>
+        <div className={classes.copyRigthDiv}>
           <p>Copyright © 2020 Thi Programmer Group</p>
           <p>Powered by Shopify</p>
         </div>
@@ -91,7 +96,7 @@ const styles = ({palette, breakpoints, spacing,typography}) => ({
   footerContainer: {
     width: '100%',
     position: 'absolute',
-    top: 400,
+    top: '70%',
     padding: '20px',
     color:'#4d4d4d',
    
@@ -109,11 +114,11 @@ const styles = ({palette, breakpoints, spacing,typography}) => ({
 },
 footerBtn:{
   border: 'none',
-  paddingBottom: '10px',
+  padding: '10px 0',
   backgroundColor: 'transparent', 
   fontSize: '1.5rem',
   fontWeight: 600,
-  color:'#4d4d4d;',
+  color:'#4d4d4d',
   fontSize: '1.2rem'
 },
 list: {
@@ -127,17 +132,19 @@ list: {
 },
 iconList: {
   display: 'flex',
-    flexDirection: 'row'
+  flexDirection: 'row',
 },
 subRow: {
   [breakpoints.down('sm')]: {
    textAlign: 'left',
-   marginRight: '60px',
-   borderBottom: 'solid 2px #f5f5f5',
+   borderBottom: 'solid 1px #d3d3d3',
     }
 },
 subRowNoDropdown: {
-textAlign: 'left'
+textAlign: 'left',
+[breakpoints.down('sm')]: {
+borderBottom: 'solid 1px #d3d3d3'
+}
 },
 dropdownIcon: {
   display: 'none',
@@ -155,7 +162,11 @@ signUpInput: {
   color: '#4d4d4d',
   fontWeight: 400,
   padding: '1.5rem 0.625rem 0.375rem',
-  width: '80%'
+  width: '80%',
+  [breakpoints.down('sm')]: {
+    width: '50%',
+    marginRight: '10px'
+    }
 
 },
 signUpBtn: {
@@ -170,6 +181,20 @@ signUpBtn: {
 },
 textSubscribe: {
   width: '90%'
+},
+subIconList: {
+  marginRight: '2%'
+},
+subscribeSignUp: {
+  [breakpoints.down('sm')]: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    marginBottom: '10px'
+    }
+},
+copyRigthDiv: {
+position: 'relative',
+bottom: '0'
 }
 });
 
