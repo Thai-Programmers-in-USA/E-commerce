6e8e7f7acdb13044336b8db3e9ffc875c1ebaeaa
BayLadyCoder <rachada_32@hotmail.com>
2021-02-26 15:30:53 -0500

    fixed import bug

--- src/components/Features/FeaturedCollection.js
+++ src/components/Features/FeaturedCollection.js
@@ -1,68 +1,66 @@
-import React from 'react'
-import ProductCard from '../ProductCard'
-import productData from '../ProductCard/productData'
-import withStyles from '@material-ui/core/styles/withStyles';
-import Typography from '@material-ui/core/Typography';
-import FeaturedCollectionSecond from './FeaturedCollectionSecond';
+import React from "react";
+import ProductCard from "../ProductCard";
+import ProductData from "../ProductCard/ProductData";
+import withStyles from "@material-ui/core/styles/withStyles";
+import Typography from "@material-ui/core/Typography";
+import FeaturedCollectionSecond from "./FeaturedCollectionSecond";
 // import Card from '@material-ui/core/Card';
 
+const FeaturedCollection = ({ classes }) => {
+  return (
+    <div className={classes.container}>
+      <Typography variant="subtitle2" component="p" className={classes.title}>
+        Featured Collection
+      </Typography>
+      <div className={classes.cardsContainer}>
+        {ProductData.map((product, index) => {
+          if (index !== ProductData.length - 1)
+            return <ProductCard product={product} key={index} />;
+        })}
+      </div>
+      <div className={classes.cardsContainerBottom}>
+        <FeaturedCollectionSecond
+          product={ProductData[ProductData.length - 1]}
+        />
+      </div>
+    </div>
+  );
+};
 
+const styles = ({ palette, breakpoints }) => ({
+  container: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "stretch",
+    flexDirection: "column",
 
- const FeaturedCollection = ({classes}) => {
-
-
-    return (
-        <div className={classes.container}>
-            <Typography variant="subtitle2" component="p" className={classes.title}>Featured Collection</Typography>
-            <div className={classes.cardsContainer}>
-                {productData.map((product, index) => {
-                    if(index !== productData.length-1)
-                        return <ProductCard product={product} key={index} />
-                }
-                )}
-            </div>
-            <div className={classes.cardsContainerBottom}>
-                <FeaturedCollectionSecond product={productData[productData.length-1]}/>
-            </div>
-           
-        </div>
-    )
-}
-
-const styles = ({palette, breakpoints}) => ({
-    container: {
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "stretch",
-        flexDirection: "column",
-
-        [breakpoints.down('sm')]: {
-            alignItems: 'center',
-        }
+    [breakpoints.down("sm")]: {
+      alignItems: "center",
     },
-    cardsContainer: {
-        display: "grid",
-        gridTemplateColumns: "repeat(5, 1fr)",
-        gap: "10px",
-        width: "100%",
-        height: "500px",
+  },
+  cardsContainer: {
+    display: "grid",
+    gridTemplateColumns: "repeat(5, 1fr)",
+    gap: "10px",
+    width: "100%",
+    height: "500px",
 
-        // "&hover": {
-        //     height: "1200px",
-        //     position: "absolute",
-        //     zIndex: 1
-        // },
-        [breakpoints.down('sm')]: {
-            overflowX: "scroll",
-        }
+    // "&hover": {
+    //     height: "1200px",
+    //     position: "absolute",
+    //     zIndex: 1
+    // },
+    [breakpoints.down("sm")]: {
+      overflowX: "scroll",
     },
+  },
 
-    title: {
-        fontSize: 20,
-        marginBottom: "2%",
-        fontWeight: 700,
-        color: palette.secondary.main
-    }
-})
+  title: {
+    fontSize: 20,
+    marginBottom: "2%",
+    fontWeight: 700,
+    color: palette.secondary.main,
+  },
+});
 
 export default withStyles(styles)(FeaturedCollection);
