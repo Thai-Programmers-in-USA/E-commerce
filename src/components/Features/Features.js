d1a7f1e0b43d702d5765f8cf80ad652dc668c993
BayLadyCoder <rachada_32@hotmail.com>
2021-02-26 16:03:51 -0500

    removed unused variables

--- src/components/Features/Features.js
+++ src/components/Features/Features.js
@@ -1,24 +1,22 @@
-import React from 'react'
-import FeaturedCategories from './FeaturedCategories'
-import FeaturedSales from './FeaturedSales'
-import FeaturedCollection from './FeaturedCollection'
-import withStyles from '@material-ui/core/styles/withStyles';
+import React from "react";
+import FeaturedCollection from "./FeaturedCollection";
+import withStyles from "@material-ui/core/styles/withStyles";
 
-const Features = ({classes}) => {
-    return (
-        <div className={classes.container}>
-            {/* <FeaturedCategories />
+const Features = ({ classes }) => {
+  return (
+    <div className={classes.container}>
+      {/* <FeaturedCategories />
             <FeaturedSales /> */}
-            <FeaturedCollection />
-        </div>
-    )
-}
+      <FeaturedCollection />
+    </div>
+  );
+};
 
-const styles =({palette, breakpoints}) => ({
-    container: {
-        display: "flex",
-        flexDirection: "column",
-    }
-})
+const styles = ({ palette, breakpoints }) => ({
+  container: {
+    display: "flex",
+    flexDirection: "column",
+  },
+});
 
 export default withStyles(styles)(Features);
