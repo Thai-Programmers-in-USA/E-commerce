d94532b729427a681f95f0adaddea5fb1034ecc1
Kato <kubkate@gmail.com>
2021-03-11 10:40:39 -0800

    clean up unwanted code

--- src/components/Features/FeaturedCollectionSecond.js
+++ src/components/Features/FeaturedCollectionSecond.js
@@ -3,10 +3,6 @@ import withStyles from "@material-ui/core/styles/withStyles";
 import Typography from "@material-ui/core/Typography";
 import Card from "@material-ui/core/Card";
 import PropTypes from "prop-types";
-// import Button from "@material-ui/core/Button";
-// import InputLabel from "@material-ui/core/InputLabel";
-// import FormControl from "@material-ui/core/FormControl";
-// import Select from "@material-ui/core/Select";
 import SaleTag from "../ProductCard/SaleTag";
 import SocialIcon from "../ProductCard/SocialIcon";
 import { Link } from 'react-router-dom';
@@ -82,44 +78,6 @@ const FeaturedCollectionSecond = ({ product, classes }) => {
           showQuantityDropdown={true}
           showAddToCartBtn={true}
         />
-
-        {/* <div className={classes.btnContainer}>
-          <FormControl variant="outlined" className={classes.quantityContainer}>
-            <InputLabel htmlFor="outlined-age-native-simple">
-              Quantity
-            </InputLabel>
-            <Select
-              className={classes.btnSelect}
-              native
-              value={1}
-              onChange={(e) => console.log(e.target.value)}
-              label="Quantity"
-              inputProps={{
-                name: "Quantity",
-                id: "outlined-age-native-simple",
-              }}
-            >
-              {quantity.map((quantity, index) => (
-                <option value={quantity} key={index}>
-                  {quantity}
-                </option>
-              ))}
-            </Select>
-          </FormControl>
-          <Button
-            className={classes.btnAddToCart}
-            variant="contained"
-            color="primary"
-          >
-            <Typography
-              className={classes.btnAddToCartText}
-              variant="subtitle2"
-              component="p"
-            >
-              Add to cart
-            </Typography>
-          </Button>
-        </div> */}
         
         <Link to={`/product/${product.slug}`} className={classes.link}>
           <Typography className={classes.fullDetailText} variant="body2">
@@ -241,44 +199,6 @@ const styles = ({ palette, breakpoints }) => ({
     fontSize: 20,
     fontWeight: 400,
   },
-  // btnContainer: {
-  //   marginTop: "20px",
-  //   width: "100%",
-
-  //   [breakpoints.down("xs")]: {
-  //     display: "flex",
-  //     justifyContent: "center",
-  //   },
-  // },
-  // btnSelect: {
-  //   height: "37px",
-  //   padding: "6px",
-
-  //   [breakpoints.down("sm")]: {
-  //     width: "110px",
-  //     height: "47px",
-  //   },
-  // },
-  // btnAddToCart: {
-  //   backgroundColor: palette.primary.main,
-  //   width: "50%",
-  //   textTransform: "capitalize",
-  //   fontWeight: 700,
-  //   marginLeft: "25px",
-  //   marginBottom: "25px",
-
-  //   [breakpoints.down("sm")]: {
-  //     width: "300px",
-  //     height: "47px",
-  //     marginLeft: "10px",
-  //   },
-  // },
-  // btnAddToCartText: {
-  //   [breakpoints.down("sm")]: {
-  //     textTransform: "None",
-  //     fontWeight: 600,
-  //   },
-  // },
   link: {
     color: palette.primary.main,
   },
