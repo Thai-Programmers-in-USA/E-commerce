d2022f8de1c08fa08cb544b7aed50b806ab02d2f
Kato <kubkate@gmail.com>
2021-03-10 18:06:15 -0800

    install react-router-dom and set up routes for Home and ProductDetail page

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
 import { MuiThemeProvider } from "@material-ui/core/styles";
 import theme from "./assets/theme/theme";
 import withStyles from "@material-ui/core/styles/withStyles";
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Home from "./pages/Home";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import ProductDetail from "./pages/ProductDetail"
 
 function App({ classes }) {
   return (
     <MuiThemeProvider theme={theme}>
-      <div className={classes.container}>
-        <Header />
-        <Home />
-        <Footer />
-      </div>
+      <BrowserRouter>
+        <div className={classes.container}>
+          <Header />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/product/:slug" component={ProductDetail} />
+          </Switch>
+          <Footer />
+        </div>
+      </BrowserRouter> 
     </MuiThemeProvider>
   );
 }
---
+++ src/pages/ProductDetail/ProductDetail.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ProductDetail = () => {
+  return (
+    <div>
+      this is a product detail page
+    </div>
+  )
+}
+
+export default ProductDetail
